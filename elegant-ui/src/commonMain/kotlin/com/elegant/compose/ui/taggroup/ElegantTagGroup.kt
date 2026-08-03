304517c676da9ea4753db88f4535cbed9d108408
vallind <imelos@163.com>
2026-08-03 21:12:21 +0800

    fix(taggroup): import interaction and remember symbols

--- elegant-ui/src/commonMain/kotlin/com/elegant/compose/ui/taggroup/ElegantTagGroup.kt
+++ elegant-ui/src/commonMain/kotlin/com/elegant/compose/ui/taggroup/ElegantTagGroup.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.FlowRow
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
